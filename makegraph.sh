99ad56fc97ec5ba8e6000b3f435eb251500dadda
yudai matsue <matsue@nagoya-u.ac.jp>
2019-08-21 15:07:49 +0900

    全消し

--- makegraph.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/amigos/fuel_generator_monitor
-date >> "makegraphlog.txt"
-python3 /home/amigos/fuel_generator_monitor/fuel24_v2.py 1>> "makegraphlog.txt" 2>> "makegraphlog.txt"
-python3 /home/amigos/fuel_generator_monitor/fuel120_v3.py 2>> "makegraphlog.txt"
-python3 /home/amigos/fuel_generator_monitor/fuel14day.py 2>> "makegraphlog.txt"
-python3 /home/amigos/fuel_generator_monitor/Generator24_v2.py 2>> "makegraphlog.txt"
-python3 /home/amigos/fuel_generator_monitor/Generator120_v2.py 2>> "makegraphlog.txt"
--- rsync_dir.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-path="/home/amigos/fuel_generator_monitor"
-date >> $path/rsync_log.txt
-rsync -av -e ssh --bwlimit=100 pi@172.20.0.84:/home/pi/GeneratorMonitor/data /home/amigos/fuel_generator_monitor/Generator_data/ >>$path/rsync_log.txt 2>>$path/rsync_log.txt
-rsync -av -e ssh --bwlimit=100 pi@172.20.0.84:/home/pi/FuelMonitor/data /home/amigos/fuel_generator_monitor/Fuel_data/ >>$path/rsync_log.txt 2>>$path/rsync_log.txt
