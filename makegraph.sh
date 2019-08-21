20d139ab52684e44277904af88145aeff2e23daf
yudai matsue <matsue@nagoya-u.ac.jp>
2019-08-21 15:22:52 +0900

    更新

--- makegraph.sh
+++ makegraph.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd /home/amigos/fuel_generator_monitor
 date >> "makegraphlog.txt"
-python3 /home/amigos/fuel_generator_monitor/fuel24_v2.py 1>> "makegraphlog.txt" 2>> "makegraphlog.txt"
-python3 /home/amigos/fuel_generator_monitor/fuel120_v3.py 2>> "makegraphlog.txt"
-python3 /home/amigos/fuel_generator_monitor/fuel14day.py 2>> "makegraphlog.txt"
-python3 /home/amigos/fuel_generator_monitor/Generator24_v2.py 2>> "makegraphlog.txt"
-python3 /home/amigos/fuel_generator_monitor/Generator120_v2.py 2>> "makegraphlog.txt"
+python3 /home/amigos/fuel_generator_monitor/fuel_graph24h.py 1>> "makegraphlog.txt" 2>> "makegraphlog.txt"
+python3 /home/amigos/fuel_generator_monitor/fuel_graph120h.py 2>> "makegraphlog.txt"
+python3 /home/amigos/fuel_generator_monitor/fuel_graph14days.py 2>> "makegraphlog.txt"
+
